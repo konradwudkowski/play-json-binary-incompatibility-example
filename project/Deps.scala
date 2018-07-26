30f32a16409d2e751bcac4dc3f8dec8a72111e0e
Konrad Wudkowski <wudkowski@gmail.com>
2018-07-26 15:40:33 +0100

    trying intransitive scope

--- project/Deps.scala
+++ project/Deps.scala
@@ -3,7 +3,7 @@ import sbt._
 object Deps {
 
   val scalaTest  = "org.scalatest"     %% "scalatest" % "3.0.5"
-  val playJson25 = "com.typesafe.play" %% "play-json" % "2.5.16"
+  val playJson25 = "com.typesafe.play" %% "play-json" % "2.5.16" % "compile" intransitive ()
   val playJson26 = "com.typesafe.play" %% "play-json" % "2.6.9"
 
 }
